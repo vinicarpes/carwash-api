c0a2cabd7664ae4c805da6eee7e5c9b7e8b284c9
vinicarpes <vinicdf000@gmail.com>
2025-05-07 16:55:38 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsc/fln/carwashapi/CarwashApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.fln.carwashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarwashApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsc/fln/carwashapi/CarwashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.fln.carwashapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarwashApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
